1e231f262a2ea882974a387f615b5fc4747c6e8b
unknown <BT_3W2_29@BKTECH.LOCAL>
2024-10-01 11:04:38 -0400

    final draft update

--- csswdproj/src/App.js
+++ csswdproj/src/App.js
@@ -8,14 +8,29 @@ function App() {
           <h1 id="page-title">Food</h1>
           <h3 id="page-subheader">Food is any substance consumed by an organism for nutritional support. Food is usually of plant, animal, or fungal origin and contains essential nutrients such as carbohydrates, fats, proteins, vitamins, or minerals. The substance is ingested by an organism and assimilated by the organism's cells to provide energy, maintain life, or stimulate growth. Different species of animals have different feeding behaviours that satisfy the needs of their metabolisms and have evolved to fill a specific ecological niche within specific geographical contexts.</h3>
         </header>
+        <nav>
+          <ul>
+            {
+              r_data.map((data, idx) => {
+                  return (
+                    <li key={idx}><a href={`#${data.name}`}>{data.name}</a></li>
+                  )
+                }
+              )
+            }
+          </ul>
+        </nav>
         <article>
-          <h2 id="header2">Food Nearby Tech</h2>
+          <h2 id="header2">Food Near Tech</h2>
           {
             r_data.map((data, idx) =>
               <Restaurant restaurant_data={data} key={idx}/>
             )
           }
         </article>
+        <footer>
+          &copy; Benjamin Tso
+        </footer>
     </div>
   );
 }
